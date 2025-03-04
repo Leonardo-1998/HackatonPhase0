94d036c5d837b9a99d3060630b310d30a2bf5dd7
leonardo-1998 <leonardo.agst98@gmail.com>
2025-03-04 08:13:28 +0700

    Fix : Pemisahan data di file JS yang berbeda

--- index.js
+++ index.js
@@ -1,67 +1,5 @@
-let name = prompt("Please enter your name")
-let data = [
-    {
-        judulLagu : "Blinding Lights",
-        artist : "The Weeknd",
-        link : "https://www.youtube.com/watch?v=fHI8X4OXluQ",
-        id: 0
-    },
-    {
-        judulLagu : "Shape of You",
-        artist : "Ed Sheeran",
-        link : "https://www.youtube.com/watch?v=JGwWNGJdvx8",
-        id : 1
-    },
-    {
-        judulLagu : "Bohemian Rhapsody",
-        artist : "Queen",
-        link : "https://www.youtube.com/watch?v=fJ9rUzIMcZQ",
-        id : 1
-    },
-    {
-        judulLagu : "Billie Jean",
-        artist : "Michael Jackson",
-        link : "https://www.youtube.com/watch?v=Zi_XLOBDo_Y",
-        id : 1
-    },
-    {
-        judulLagu : "Smells Like Teen Spirit",
-        artist : "Nirvana",
-        link : "https://www.youtube.com/watch?v=hTWKbfoikeg",
-        id : 1
-    },
-    {
-        judulLagu : "Someone Like You",
-        artist : "Adele",
-        link : "https://www.youtube.com/watch?v=hLQl3WQQoQ0",
-        id : 1
-    },
-    {
-        judulLagu : "Believer",
-        artist : "Imagine Dragons",
-        link : "https://www.youtube.com/watch?v=7wtfhZwyrcc",
-        id : 1
-    },
-    {
-        judulLagu : "Hotel California",
-        artist : "Eagles",
-        link : "https://www.youtube.com/watch?v=EqPtz5qN7HM",
-        id : 1
-    },
-    {
-        judulLagu : "Rolling in the Deep",
-        artist : "Adele",
-        link : "https://www.youtube.com/watch?v=rYEDA3JcQqw",
-        id : 1
-    },
-    {
-        judulLagu : "Take On Me",
-        artist : "a-ha",
-        link : "https://www.youtube.com/watch?v=djV11Xbc914",
-        id : 1
-    }
+// let name = prompt("Please enter your name")
 
-]
 
 function submit() {
     // Mengambil property text input
@@ -83,7 +21,8 @@ function submit() {
     let objectOfPlaylist = {
         judulLagu: judulLagu.value,
         artis: artis.value,
-        link: link.value
+        link: link.value,
+        id : data.length
     }
     // Mempush data yang diambil kedalam array of object
     data.push(objectOfPlaylist)
